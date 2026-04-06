bb7c04594d891d9574ed9120f8a77b19b0789121
Shelly <shelly@gkmit.co>
2026-04-06 11:12:55 +0530

    refactor: remove aggrid table

--- src/webhook/listItem.tsx
+++ src/webhook/listItem.tsx
@@ -103,7 +103,7 @@ export default function WebhookListItem({
         hour: "2-digit",
         minute: "2-digit",
       });
-    } catch () {
+    } catch (error) {
       return "Invalid date";
     }
   };
